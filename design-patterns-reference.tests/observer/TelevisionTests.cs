8e9cac60e1541d936303a084e278b894d1bc098f
Joseph P. Short <josephpshort@gmail.com>
2016-03-27 12:48:37 -0400

    observer pattern

---
+++ design-patterns-reference.tests/observer/TelevisionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using design_patterns_reference.observer;
+using Moq;
+
+namespace design_patterns_reference.tests.observer
+{
+    [TestClass]
+    public class TelevisionTests
+    {
+        [TestMethod]
+        public void RemoteChannelChanged_ShouldStoreHistoryInTelevision()
+        {
+            var results = new List<int>();
+            var television = new Television(results);
+            var remote = new TelevisionRemote();
+            remote.ChannelChanged += television.ChannelChanged;
+
+            for (var i = 0; i < 10; i++)
+            {
+                remote.Channel = i;
+            }
+
+            Assert.AreEqual(10, results.Count);
+        }
+    }
+}
---
+++ design-patterns-reference/observer/Television.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace design_patterns_reference.observer
+{
+    public class TelevisionRemote
+    {
+        public delegate void ChannelChangedDelegate(int newChannel);
+        
+        public event ChannelChangedDelegate ChannelChanged;
+        
+        private int _channel;
+
+        public int Channel
+        {
+            set
+            {
+                _channel = value;
+                ChannelChanged?.Invoke(_channel);
+            }
+        }
+    }
+
+    public class Television
+    {
+        private readonly IList<int> _channelHistory; 
+
+        public Television(IList<int> channelHistory)
+        {
+            _channelHistory = channelHistory;
+        }
+
+        public void ChannelChanged(int newChannel)
+        {
+            _channelHistory?.Add(newChannel);
+        }
+    }
+}
